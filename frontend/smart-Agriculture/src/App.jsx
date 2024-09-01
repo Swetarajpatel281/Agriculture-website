b1ff20144f5b1e95a4a02094fccb0b9b051af045
Sweta Raj patel <swetarajpatel2803@gmail.com>
2024-09-01 13:47:52 +0530

    add

--- frontend/smart-Agriculture/src/App.jsx
+++ frontend/smart-Agriculture/src/App.jsx
@@ -1,5 +1,5 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import './App.css'
+// import './App.css'
 import DiseaseRecognition from './Components/DiseaseRecognition';
 // import Footer from './Components/Footer';
 import MarketUpdate from './Components/MarketUpdate';
